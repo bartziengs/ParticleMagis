1bff005416df087df6b2cc7baa0ce61e1fb91ad1
MilleIT <m.j.e.vanosch@student.tudelft.nl>
2017-12-03 21:38:00 +0100

    the nodemailer werkt met form validation(standaard html5, had geen puf meer) in deze commit zit ook de express backend, maar deze map moet nog een keer gemerged worden met de particlemagis map voor overzicht

--- src/Components/form.js
+++ src/Components/form.js
@@ -1,97 +1,157 @@
 import React, { Component } from 'react';
-import nodemailer from 'nodemailer';
 
 /**
  * @author Millen van Osch
  * Contact form in jsx sytanx
  */
 
+/**
+ * replaces the form when it has been submitted and successfully send.
+ */
+class FormSent extends Component {
+    render = () => {
+        return (
+            <section id="form">
+                <div className="container" id="Register">
+                    <h1 className="well" style={{textAlign: 'center'}}>Thank you! We'll be in touch with you soon!</h1>
+                </div>
+            </section>
+        );
+    }
+}
+
+/**
+ * replaces the form when it has been submitted but couldn't be send because of failure
+ */
+class FormFailed extends Component {
+    render = () => {
+        return (
+            <section id="form">
+                <div className="container" id="Register">
+                    <h1 className="well" style={{textAlign: 'center'}}>Sorry something went wrong, please refresh and try again later</h1>
+                </div>
+            </section>
+        );
+    }
+}
 
 class FormComponent extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
+    this.handleRendering = this.handleRendering.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.form = this.form.bind(this);
+    this.state = {formSent: null};
   }
 
+    /**
+     * handles the conditional rendering
+     * @returns the form
+     */
+  handleRendering = () => {
+    if (this.state.formSent === null) {
+        return this.form()
+    } else {
+        if (this.state.formSent) {
+            return <div><FormSent /></div>
+        } else {
+            return <div><FormFailed /></div>
+        }
+    }
+  }
+  /**
+   * collects the data from the form and passes it to the express backend where it can be mailed
+   */
   handleSubmit(event){
     event.preventDefault();
     console.log("sendin data");
-    var formData = {
-        name: document.getElementsByName("name")[0].value,
-        email: document.getElementsByName("email")[0].value,
-        subject: document.getElementsByName("subject")[0].value,
-        message: document.getElementsByName("message")[0].value
-    };
-    let transporter = nodemailer.createTransport({
-        sendmail: true,
-        newline: 'unix',
-        path: '/usr/sbin/sendmail'
-    });
-    transporter.sendMail({
-        from: formData.email,
-        to: 'millen_vanosch@hotmail.com',
-        subject: formData.subject,
-        text: formData.message
-    }, (err, info) => {
-        console.log(info.envelope);
-        console.log(info.messageId);
+
+    fetch('/send', {
+      method: 'POST',
+      headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+          name: document.getElementsByName('name')[0].value,
+          email: document.getElementsByName('email')[0].value,
+          subject: document.getElementsByName('subject')[0].value,
+          message: document.getElementsByName('message')[0].value
+      })
+    }).then((response) => response.json()).then((responseJson) => {
+        if (responseJson.success) {
+            this.setState({formSent: true})
+            console.log('form is sent')
+        } else {
+            this.setState({formSent: false})
+            console.log('form is not sent')
+        }
+    }).catch((error) => {
+        console.error(error);
+        this.setState({formSent: false})
     });
   }
 
+  /**
+   * the original form
+   */
+  form() {
+    return (
+        <section id="form">
+            <div className="container" id="Register">
+                <h1 className="well" style={{textAlign: 'center'}}>Get in touch with us!</h1>
+                <div className="col-lg-12 well">
+                    <div className="row">
+                        <form onSubmit={this.handleSubmit}>
+                            <div className="col-sm-12">
+                                <div className="row">
+                                    <div className="col-sm-6 form-group">
+                                        <label>Your Name</label>
+                                        <input className="form-control" name="name" placeholder="Enter full name here.." type="text" minlength="4" required/>
+                                    </div>
+                                    <div className="col-sm-6 form-group">
+                                        <label>Your Email</label>
+                                        <input className="form-control" name="email" placeholder="Enter Email here.." type="email" required/>
+                                    </div>
+                                </div>
+                                {/* <div className="row">
+                                    <div className="col-sm-4 form-group">
+                                      <label>City</label>
+                                      <input className="form-control" placeholder="Enter city name here.." type="text" />
+                                    </div>
+                                    <div className="col-sm-4 form-group">
+                                      <label>ZIP</label>
+                                      <input className="form-control" placeholder="Enter ZIP code Here.." type="text" />
+                                    </div>
+                                    <div className="col-sm-4 form-group">
+                                      <label>House number</label>
+                                      <input className="form-control" placeholder="Enter house number Here.." type="text" />
+                                    </div>
+                                  </div>
+                                  <div className="form-group">
+                                    <label>Phone Number</label>
+                                    <input className="form-control" placeholder="Enter phone number here.." type="text" />
+                                  </div> */}
+                                <div className="form-group">
+                                    <label>Subject</label>
+                                    <input className="form-control" name="subject" placeholder="What is the subject?" type="text" />
+                                </div>
+                                <div className="form-group">
+                                    <label>Message</label>
+                                    <textarea className="form-control" name="message" placeholder="Leave a message for us here.." rows="8" minlength="8" required/>
+                                </div>
+                                <button className="btn btn-lg formbutton" type="submit">Submit</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+  }
 
   render = () => {
-    return (
-      <section id="form">
-        <div className="container" id="Register">
-          <h1 className="well">Stay in contact with us!</h1>
-          <div className="col-lg-12 well">
-            <div className="row">
-              <form onSubmit={this.handleSubmit}>
-                <div className="col-sm-12">
-                  <div className="row">
-                    <div className="col-sm-6 form-group">
-                      <label>Your Name</label>
-                      <input className="form-control" name="name" placeholder="Enter full name here.." type="text" />
-                    </div>
-                    <div className="col-sm-6 form-group">
-                      <label>Your Email</label>
-                      <input className="form-control" name="email" placeholder="Enter Email here.." type="text" data-rule-email="true" data-msg="Please enter a valid email" />
-                    </div>
-                  </div>
-                  {/* <div className="row">
-                    <div className="col-sm-4 form-group">
-                      <label>City</label>
-                      <input className="form-control" placeholder="Enter city name here.." type="text" />
-                    </div>
-                    <div className="col-sm-4 form-group">
-                      <label>ZIP</label>
-                      <input className="form-control" placeholder="Enter ZIP code Here.." type="text" />
-                    </div>
-                    <div className="col-sm-4 form-group">
-                      <label>House number</label>
-                      <input className="form-control" placeholder="Enter house number Here.." type="text" />
-                    </div>
-                  </div>
-                  <div className="form-group">
-                    <label>Phone Number</label>
-                    <input className="form-control" placeholder="Enter phone number here.." type="text" />
-                  </div> */}
-                  <div className="form-group">
-                    <label>Subject</label>
-                    <input className="form-control" name="subject" placeholder="What is the subject?" type="text" />
-                  </div>
-                  <div className="form-group">
-                    <label>Message</label>
-                    <textarea className="form-control" name="message" placeholder="Leave a message for us here.." rows="8" data-rule-required="true" data-msg="Please leave a message for us" />
-                  </div>
-                  <button className="btn btn-lg btn-info" type="submit">Submit</button>
-                </div>
-              </form>
-            </div>
-          </div>
-        </div>
-      </section>
-    );
+      return this.handleRendering()
   }
 }
 
