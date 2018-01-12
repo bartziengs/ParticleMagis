631e02a575da7e154f19a516e7db0305493fd6f0
LMudde <lars.mudde@hotmail.com>
2018-01-12 12:07:24 +0100

    LM >> Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  entry: [
+    './src/index.js'
+  ],
+  output: {
+    path: __dirname,
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [{
+      exclude: /node_modules/,
+      loader: 'babel',
+      query: {
+        presets: ['react', 'es2016', 'stage-1']
+      }
+    }]
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  devServer: {
+    historyApiFallback: true,
+    inline: true,
+    contentBase: './',
+    disableHostCheck: true,
+    host: '0.0.0.0',
+    port: 3000
+  }
+};
