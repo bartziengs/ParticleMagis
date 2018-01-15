aa1f6ad33ed7b1ec6fae141b402c0b90d46f89ca
NB-LARS\LarsM <lars.mudde@hotmail.com>
2018-01-15 11:17:00 +0100

    LM >> Check

---
+++ ExpressMagis/client/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import registerServiceWorker from './registerServiceWorker';
+import HomeComponent from './Components/home';
+
+
+ReactDOM.render(<HomeComponent />, document.getElementById('particles'));
+          
+
+registerServiceWorker();
