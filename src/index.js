c9344e0e5760101f5fcfde4976bbcf4fd6b66155
NB-LARS\LarsM <lars.mudde@hotmail.com>
2018-01-15 16:36:37 +0100

    LM >> Router broken!! Anyone please fix this XD SOS

--- src/index.js
+++ src/index.js
@@ -2,24 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import HashRouter from 'react-router-dom';
 import HomeComponent from './Components/Home/HomeComponent';
 import PPComponent from './Components/Footer/PPComponent';
 import TermsComponent from './Components/Footer/TermsComponent';
 
-
-
 let applicationTarget = document.getElementById('particles');
 
-
 ReactDOM.render(
-    <Router>
+    <HashRouter>
         <div>
         <Route exact path="/" component={HomeComponent} />
         <Route path="/privacy" component={PPComponent} />
         <Route path="/terms" component={TermsComponent} />
         </div>
-    </Router>,
+    </HashRouter>,
     applicationTarget
 );
 
