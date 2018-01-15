f8f1c4a38c1edb49f7d089e576d11f31a5a8c63f
NB-LARS\LarsM <lars.mudde@hotmail.com>
2018-01-15 01:02:29 +0100

    LM >> Transformed to webpack, routing issue remains

--- webpack.config.js
+++ webpack.config.js
@@ -8,16 +8,17 @@ module.exports = {
     filename: 'bundle.js'
   },
   module: {
-  noParse: [/.*(pixi\.js).*/],
-  rules: [
-    {
-      test: /\.js$/,
-      exclude: /(node_modules|bower_components)/,
-      use: {
-        loader: 'babel-loader',
-      }
-    }
-  ]},
+    loaders: [{
+      test: /.jsx?$/,
+      loader: 'babel-loader',
+      exclude: /node_modules/
+    }, {
+      test: /\.css$/,
+      loader: "style-loader!css-loader"
+    }, {
+      test: /\.(jpe?g|png|gif|woff|woff2|eot|ttf|svg)(\?[a-z0-9=.]+)?$/,
+      loader: 'url-loader?limit=100000' }]
+  },
   resolve: {
     extensions: ['.js', '.jsx']
   },
