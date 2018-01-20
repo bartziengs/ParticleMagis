88fcb40e5e4d120bd72cb9802a53b03e65051e41
MilleIT <millen_vanosch@hotmail.com>
2018-01-20 17:05:51 +0100

    veel plezier het wertk niet

--- src/Components/Footer/FooterComponent.js
+++ src/Components/Footer/FooterComponent.js
@@ -12,20 +12,23 @@ class FooterComponent extends Component {
     render() {
 
         return (
-            <div className='footer' id="foot">
-                <div className="container">
-                    <div className="row infotext stepsinfotext">
-                        <div className="col-lg-4 col-md-6 col-xs-12 text-center footer-text">
-                            <Link to="/privacy">Privacy Policy</Link>
-                        </div>
-                        <div className="col-lg-4 col-md-6 col-xs-12 text-center footer-text">
-                            <Link to="/terms">Terms of Service</Link>
-                        </div>
-                        <div className="col-lg-4 col-md-6 col-xs-12 text-center footer-text">
-                            KvK Nummer: 68904657
+            <div>
+                <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSd8AJFccC-F4XEvrF2EhTE9G5hhzFLI8zbWsI5xWIAVPhXscQ/viewform?embedded=true" width="760" height="500" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>
+                    <div className='footer' id="foot">
+                        <div className="container">
+                            <div className="row infotext stepsinfotext">
+                                <div className="col-lg-4 col-md-6 col-xs-12 text-center footer-text">
+                                    <Link to="/privacy">Privacy Policy</Link>
+                                </div>
+                                <div className="col-lg-4 col-md-6 col-xs-12 text-center footer-text">
+                                    <Link to="/terms">Terms of Service</Link>
+                                </div>
+                                <div className="col-lg-4 col-md-6 col-xs-12 text-center footer-text">
+                                    KvK Nummer: 68904657
+                                </div>
+                            </div>
                         </div>
                     </div>
-                </div>
             </div>
         );
     }
